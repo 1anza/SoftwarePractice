d12740e19b3da93f2208ee20497eda6a7fac7cae
1anza <89605881+1anza@users.noreply.github.com>
2023-01-03 17:18:31 -0700

    Add files via upload

---
+++ Spreadsheet/DependencyGraph/DependencyGraph.cs
@@ -0,0 +1,336 @@
+﻿// Skeleton implementation written by Joe Zachary for CS 3500, September 2013.
+// Version 1.1 (Fixed error in comment for RemoveDependency.)
+// Version 1.2 - Daniel Kopta 
+//               (Clarified meaning of dependent and dependee.)
+//               (Clarified names in solution/project structure.)
+// Completed Verison by Alivia Liljenquist
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpreadsheetUtilities
+{
+
+    /// <summary>
+    /// (s1,t1) is an ordered pair of strings
+    /// t1 depends on s1; s1 must be evaluated before t1
+    /// 
+    /// A DependencyGraph can be modeled as a set of ordered pairs of strings.  Two ordered pairs
+    /// (s1,t1) and (s2,t2) are considered equal if and only if s1 equals s2 and t1 equals t2.
+    /// Recall that sets never contain duplicates.  If an attempt is made to add an element to a 
+    /// set, and the element is already in the set, the set remains unchanged.
+    /// 
+    /// Given a DependencyGraph DG:
+    /// 
+    ///    (1) If s is a string, the set of all strings t such that (s,t) is in DG is called dependents(s).
+    ///        (The set of things that depend on s)    
+    ///        
+    ///    (2) If s is a string, the set of all strings t such that (t,s) is in DG is called dependees(s).
+    ///        (The set of things that s depends on) 
+    //
+    // For example, suppose DG = {("a", "b"), ("a", "c"), ("b", "d"), ("d", "d")}
+    //     dependents("a") = {"b", "c"}
+    //     dependents("b") = {"d"}
+    //     dependents("c") = {}
+    //     dependents("d") = {"d"}
+    //     dependees("a") = {}
+    //     dependees("b") = {"a"}
+    //     dependees("c") = {"a"}
+    //     dependees("d") = {"b", "d"}
+    /// </summary>
+    public class DependencyGraph
+    {
+        private Dictionary<string, HashSet<string>> dependents;
+        private Dictionary<string, HashSet<string>> dependees;
+
+        /// <summary>
+        /// Creates an empty DependencyGraph.
+        /// </summary>
+        public DependencyGraph()
+        {
+            dependents = new Dictionary<string, HashSet<string>>();
+            dependees = new Dictionary<string, HashSet<string>>();
+        }
+
+
+        /// <summary>
+        /// The number of ordered pairs in the DependencyGraph.
+        /// Loops through dependents - for every dependee listed in dependent value, add 1
+        /// </summary>
+        public int Size
+        {
+            get 
+            {
+                int s = 0;
+                foreach (KeyValuePair<string, HashSet<string>>kvp in dependents)
+                {
+                    foreach (string dependee in kvp.Value)
+                    {
+                        s++;
+                    }
+                }
+                return s;
+            }
+        }
+
+
+        /// <summary>
+        /// The size of dependees(s).
+        /// This property is an example of an indexer.  If dg is a DependencyGraph, you would
+        /// invoke it like this:
+        /// dg["a"]
+        /// It should return the size of dependees("a")
+        /// Gets dependents of a string
+        /// </summary>
+        public int this[string s]
+        {
+            get
+            {
+                HashSet<string> getDependents;
+                if (dependees.TryGetValue(s, out getDependents))
+                {
+                    return getDependents.Count;
+                }
+                return 0;
+            }
+        }
+
+
+        /// <summary>
+        /// Reports whether dependents(s) is non-empty.
+        /// Is there anything that depends on s? True or False
+        /// </summary>
+        public bool HasDependents(string s)
+        {
+            if (dependents.ContainsKey(s))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Reports whether dependees(s) is non-empty.
+        /// Is there anything that s depends on? True or False
+        /// </summary>
+        public bool HasDependees(string s)
+        {
+            if (dependees.ContainsKey(s))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Enumerates dependents(s).
+        /// List of things that depend on s, else return empty
+        /// </summary>
+        public IEnumerable<string> GetDependents(string s)
+        {
+            HashSet<string> getDependees;
+            if (dependents.TryGetValue(s, out getDependees))
+            {
+                return getDependees;
+            }
+            else
+            {
+                return new HashSet<string>();
+            }
+        }
+
+        /// <summary>
+        /// Enumerates dependees(s).
+        /// List of things s depends on, else return empty
+        /// </summary>
+        public IEnumerable<string> GetDependees(string s)
+        {
+            HashSet<string> getDependents;
+            if (dependees.TryGetValue(s, out getDependents))
+            {
+                return getDependents;
+            }
+            else
+            {
+                return new HashSet<string>();
+            }
+        }
+
+
+        /// <summary>
+        /// <para>Adds the ordered pair (s,t), if it doesn't exist</para>
+        /// 
+        /// <para>This should be thought of as:</para>   
+        /// 
+        ///   t depends on s
+        ///
+        /// </summary>
+        /// <param name="s"> s must be evaluated first. T depends on S</param>
+        /// <param name="t"> t cannot be evaluated until s is</param>        /// 
+        public void AddDependency(string s, string t)
+        {
+            if (s == null || t == null)
+            {
+                throw new ArgumentException("Incorrect inserted values in AddDependency.");
+            }
+
+            /// Adds t as something that depends on s in dependents dictionary - "s":{"t"}
+            /// If nothing depends on s ...
+            if (!HasDependents(s))
+            {
+                HashSet<string> dep = new HashSet<string>();
+                dep.Add(t);
+                dependents.Add(s, dep);
+            }
+            else
+            {
+                HashSet<string> getDependees;
+                if (dependents.TryGetValue(s, out getDependees))
+                {
+                    if (!getDependees.Contains(t))
+                    {
+                        getDependees.Add(t);
+                    }
+                }
+            }
+
+            /// Adds s as something t depends on in dependees dictionary - "t":{"s"}
+            /// If s does not depend on something...
+            if (!HasDependees(t))
+            {
+                HashSet<string> dep2 = new HashSet<string>();
+                dep2.Add(s);
+                dependees.Add(t, dep2);
+            }
+            else
+            {
+                HashSet<string> getDependents;
+                if (dependees.TryGetValue(t, out getDependents))
+                {
+                    if (!getDependents.Contains(s))
+                    {
+                        getDependents.Add(s);
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Removes the ordered pair (s,t), if it exists
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        public void RemoveDependency(string s, string t)
+        {
+            if (s == null || t == null)
+            {
+                throw new ArgumentException("Incorrect inserted values in RemoveDependency.");
+            }
+            /// Removes t from s key in dependent dictionary
+            HashSet<string> getDependees;
+            if (dependents.TryGetValue(s, out getDependees))
+            {
+                getDependees.Remove(t);
+                if (getDependees.Count == 0)
+                {
+                    dependents.Remove(s);
+                }
+            }
+
+            /// Removes s from t key in dependee dictionary
+            HashSet<string> getDependents;
+            if (dependees.TryGetValue(t, out getDependents))
+            {
+                getDependents.Remove(s);
+                if (getDependents.Count == 0)
+                {
+                    dependees.Remove(t);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Removes all existing ordered pairs of the form (s,r).  Then, for each
+        /// t in newDependents, adds the ordered pair (s,t).
+        /// </summary>
+        public void ReplaceDependents(string s, IEnumerable<string> newDependents)
+        {
+            HashSet<string> getDependees;
+            if (dependents.TryGetValue(s, out getDependees))
+            {
+                string[] oldDdees = getDependees.ToArray();
+                
+                // Removes s from dependent sets in dependees dictionary
+                foreach (string oldDdee in oldDdees)
+                {
+                    HashSet<string> oldDdeesHash;
+                    if (dependees.TryGetValue(oldDdee, out oldDdeesHash))
+                    {
+                        oldDdeesHash.Remove(s);
+                    }
+                }
+
+                // Clears dependee set in s dependent dictionary and add things that depend on it
+                getDependees.Clear();
+                foreach (string newDdent in newDependents)
+                {
+                    getDependees.Add(newDdent);
+                }
+            }
+
+            foreach (string newDdent in newDependents)
+            {
+                AddDependency(s, newDdent);
+            }
+        }
+
+
+        /// <summary>
+        /// Removes all existing ordered pairs of the form (r,s).  Then, for each 
+        /// t in newDependees, adds the ordered pair (t,s).
+        /// </summary>
+        public void ReplaceDependees(string s, IEnumerable<string> newDependees)
+        {
+            HashSet<string> getDependents;
+            if (dependees.TryGetValue(s, out getDependents))
+            {
+                string[] prevDependents = getDependents.ToArray();
+                getDependents.Clear();
+
+                foreach (string prevDependent in prevDependents)
+                {
+                    HashSet<string> prevDependentHash;
+                    if (dependents.TryGetValue(prevDependent, out prevDependentHash))
+                    {
+                        prevDependentHash.Remove(s);
+                    }
+                }
+
+                foreach (string newDependent in newDependees)
+                {
+                    getDependents.Add(newDependent);
+                }
+            }
+
+            foreach (string newDependee in newDependees)
+            {
+                AddDependency(newDependee, s);
+            }
+        }
+
+    }
+
+}
+
---
+++ TipCalculator/TipCalculator/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace TipCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EnterBillLabel = new System.Windows.Forms.Label();
+            this.totalBill = new System.Windows.Forms.TextBox();
+            this.tipCompute = new System.Windows.Forms.TextBox();
+            this.ComputeBtn = new System.Windows.Forms.Button();
+            this.tipLabel = new System.Windows.Forms.Label();
+            this.TipPercent = new System.Windows.Forms.TextBox();
+            this.totalTipLabel = new System.Windows.Forms.Label();
+            this.totalCompute = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // EnterBillLabel
+            // 
+            this.EnterBillLabel.AutoSize = true;
+            this.EnterBillLabel.Location = new System.Drawing.Point(198, 76);
+            this.EnterBillLabel.Name = "EnterBillLabel";
+            this.EnterBillLabel.Size = new System.Drawing.Size(75, 13);
+            this.EnterBillLabel.TabIndex = 0;
+            this.EnterBillLabel.Text = "Enter Total Bill";
+            // 
+            // totalBill
+            // 
+            this.totalBill.Location = new System.Drawing.Point(311, 76);
+            this.totalBill.Name = "totalBill";
+            this.totalBill.Size = new System.Drawing.Size(100, 20);
+            this.totalBill.TabIndex = 1;
+            this.totalBill.TextChanged += new System.EventHandler(this.totalBill_TextChanged);
+            // 
+            // tipCompute
+            // 
+            this.tipCompute.Location = new System.Drawing.Point(311, 175);
+            this.tipCompute.Name = "tipCompute";
+            this.tipCompute.Size = new System.Drawing.Size(100, 20);
+            this.tipCompute.TabIndex = 2;
+            // 
+            // ComputeBtn
+            // 
+            this.ComputeBtn.Location = new System.Drawing.Point(198, 218);
+            this.ComputeBtn.Name = "ComputeBtn";
+            this.ComputeBtn.Size = new System.Drawing.Size(75, 23);
+            this.ComputeBtn.TabIndex = 3;
+            this.ComputeBtn.Text = "Compute";
+            this.ComputeBtn.UseVisualStyleBackColor = true;
+            this.ComputeBtn.Click += new System.EventHandler(this.ComputeBtn_Click);
+            // 
+            // tipLabel
+            // 
+            this.tipLabel.AutoSize = true;
+            this.tipLabel.Location = new System.Drawing.Point(198, 126);
+            this.tipLabel.Name = "tipLabel";
+            this.tipLabel.Size = new System.Drawing.Size(62, 13);
+            this.tipLabel.TabIndex = 4;
+            this.tipLabel.Text = "Tip Percent";
+            // 
+            // TipPercent
+            // 
+            this.TipPercent.Location = new System.Drawing.Point(311, 126);
+            this.TipPercent.Name = "TipPercent";
+            this.TipPercent.Size = new System.Drawing.Size(100, 20);
+            this.TipPercent.TabIndex = 5;
+            this.TipPercent.TextChanged += new System.EventHandler(this.TipPercent_TextChanged);
+            // 
+            // totalTipLabel
+            // 
+            this.totalTipLabel.AutoSize = true;
+            this.totalTipLabel.Location = new System.Drawing.Point(198, 175);
+            this.totalTipLabel.Name = "totalTipLabel";
+            this.totalTipLabel.Size = new System.Drawing.Size(49, 13);
+            this.totalTipLabel.TabIndex = 6;
+            this.totalTipLabel.Text = "Total Tip";
+            // 
+            // totalCompute
+            // 
+            this.totalCompute.Location = new System.Drawing.Point(311, 220);
+            this.totalCompute.Name = "totalCompute";
+            this.totalCompute.Size = new System.Drawing.Size(100, 20);
+            this.totalCompute.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.totalCompute);
+            this.Controls.Add(this.totalTipLabel);
+            this.Controls.Add(this.TipPercent);
+            this.Controls.Add(this.tipLabel);
+            this.Controls.Add(this.ComputeBtn);
+            this.Controls.Add(this.tipCompute);
+            this.Controls.Add(this.totalBill);
+            this.Controls.Add(this.EnterBillLabel);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label EnterBillLabel;
+        private System.Windows.Forms.TextBox totalBill;
+        private System.Windows.Forms.TextBox tipCompute;
+        private System.Windows.Forms.Button ComputeBtn;
+        private System.Windows.Forms.Label tipLabel;
+        private System.Windows.Forms.TextBox TipPercent;
+        private System.Windows.Forms.Label totalTipLabel;
+        private System.Windows.Forms.TextBox totalCompute;
+    }
+}
+
